99e9fab87b916b7a0323b1fb445348aee7254de1
rocampocastro604 <44033716+rocampocastro604@users.noreply.github.com>
2018-10-22 21:31:07 -0500

    Aplicacion Android
    MainActivity and ChooseActivity added

---
+++ mobile_app/panalysis/app/src/main/java/com/example/panalysis/chooseActivity.java
@@ -0,0 +1,17 @@
+package com.example.panalysis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class chooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+        setTitle("Analisis Numerico");
+
+    }
+
+}
